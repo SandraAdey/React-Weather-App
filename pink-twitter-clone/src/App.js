c5cb3c9806093b745a5fd4dcbd76b6b36bf6ea23
Sandra Adey <sandraadeyinka@gmail.com>
2023-02-19 13:15:50 +0000

    starting up react

---
+++ pink-twitter-clone/src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+function App() {
+  //const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=103666d5252e5abf754527cd2f707b91`;
+
+  return <div className="app">
+    <div className="container">
+      <div className="top"></div>
+      <div className="bottom"></div>
+    </div>
+  </div>;
+}
+
+export default App;
